c7881c9087e2d62071d57cd93fb412407dd04ba2
Ademir Guilherme <atguilherme@gmail.com>
2023-04-28 14:24:57 -0300

    feat: add chakra lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
 import { UserDashboardPage } from "./pages/UserDashboardPage";
 import { extendTheme, ChakraProvider } from '@chakra-ui/react'
 
-
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
-
-
 function App() {
 
   const theme = extendTheme({ colors })
---
+++ src/components/Header/HeaderStyle.ts
@@ -0,0 +1,9 @@
+import { chakra } from "@chakra-ui/react";
+
+export const HeaderStyle = chakra("header" , {
+  baseStyle: {
+    with: "100%",
+    height: "80px",
+    padding: "0 5%"
+  }
+})
