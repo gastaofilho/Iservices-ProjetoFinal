af4147f7930c1fe71cd8fd0730e9cb5ddc6c7a6c
Ademir Guilherme <atguilherme@gmail.com>
2023-04-28 07:52:38 -0300

    fix: remove unnecessary files

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,9 @@ import { Button, Flex } from "@chakra-ui/react";
 export const Header = () => {
   return (
     <>
-    <Flex></Flex>
+    <Flex>
+      
+    </Flex>
       <img src={Logo} alt="Logotipo" />
       <Button colorScheme="blue">Sair</Button>
     </>
--- src/pages/RegisterPage/StyledUserDashboard.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const StyledUserDashboard = styled.div`
-header{
-  display: flex;
-  width: 100%;
-  max-width: 1100px;
-}
-`;
