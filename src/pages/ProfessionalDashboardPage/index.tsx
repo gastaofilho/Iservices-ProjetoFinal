6b0162a517fa779c19b72299359231c095e3c4e1
Ademir Guilherme <atguilherme@gmail.com>
2023-05-02 22:18:25 -0300

    fix: UserContext-> autologin, ProfessionalPage ->header

--- src/pages/ProfessionalDashboardPage/index.tsx
+++ src/pages/ProfessionalDashboardPage/index.tsx
@@ -1,10 +1,25 @@
+import { Button } from "@chakra-ui/react";
+import { Header } from "../../components/Header"
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserContext";
+import { theme } from "../../styles/theme";
 
 export const ProfessionalDashboardPage = () => {
-
-
+  const { userLogout } = useContext(UserContext);
 
   return (
     <>
+    <Header>
+    <Button
+        w={"142px"}
+         h={["36px", "48px"]}
+         onClick={() => userLogout()}
+         bgColor={theme.colors.primary}
+         textColor={theme.colors.branco}
+         >
+          Sair
+        </Button>
+    </Header>
     <h1>Professional Dashboard Page</h1>
     </>
   );
