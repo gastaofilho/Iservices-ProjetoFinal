d31f927b3b9d0de45967dd946d549de30c958602
Dolfour <rodolfohenriques23@gmail.com>
2023-05-05 14:44:50 -0300

    fix: Ajuste no User Context

--- src/providers/UserDashboardContext.tsx
+++ src/providers/UserDashboardContext.tsx
@@ -11,20 +11,6 @@ interface IUserDashboardContext {
   setProfessionalList: React.Dispatch<React.SetStateAction<IProfessional[]>>;
   jobList: IJob[];
   setJobList: React.Dispatch<React.SetStateAction<IJob[]>>;
-  professionalData: IProfessionalData[];
-  loadJobList: any;
-  // setProfessionalData: React.Dispatch<React.SetStateAction<IProfessional[]>>;
-}
-
-interface IProfessionalData extends IJob, IProfessional {
-  name: string;
-  userType: string;
-  userJob: string;
-  title: string;
-  description: string;
-  contact: string;
-  category: string;
-  id: number;
 }
 
 interface IJob {
@@ -33,6 +19,7 @@ interface IJob {
   contact: string;
   category: string;
   id: number;
+  userId: string;
 }
 
 interface IProfessional {
@@ -41,6 +28,7 @@ interface IProfessional {
   userJob: string;
   contact: string;
   id: number;
+  jobs?: IJob[];
 }
 
 export const UserDashboardContext = createContext({} as IUserDashboardContext);
@@ -48,24 +36,6 @@ export const UserDashboardContext = createContext({} as IUserDashboardContext);
 export const UserDashboardProvider = ({ children }: IUserDashboardProviderProps) => {
   const [professionalList, setProfessionalList] = useState<IProfessional[]>([])
   const [jobList, setJobList] = useState<IJob[]>([])
-  const [professionalData, setProfessionalData] = useState<IProfessionalData[]>([])
-  
-  const loadJobList = async (userId:any) => {
-    const token = localStorage.getItem("@TOKEN");
-    console.log(userId)
-    try {
-      const { data } = await api.get<IJob[]>(`/jobs/${userId}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      console.log(data)
-      setJobList(data);
-    } catch (error) {
-      console.log(error)
-      toast.error("Token inválido");
-    }
-  };
 
   useEffect(() => {
     const loadProfessionalList = async () => {
@@ -75,9 +45,11 @@ export const UserDashboardProvider = ({ children }: IUserDashboardProviderProps)
           headers: {
             Authorization: `Bearer ${token}`,
           },
+          params: {
+            _embed: "jobs"
+          }
         });
         setProfessionalList(data.filter((professional) => professional.userType === "professional"));
-        professionalList.map((professional) => (loadJobList(professional.id)))
       } catch (error) {
         toast.error("Token inválido");
       }
@@ -86,7 +58,7 @@ export const UserDashboardProvider = ({ children }: IUserDashboardProviderProps)
   }, []);
 
   return (
-    <UserDashboardContext.Provider value={{ professionalList, setProfessionalList, jobList, setJobList, professionalData, loadJobList }}>
+    <UserDashboardContext.Provider value={{ professionalList, setProfessionalList, jobList, setJobList }}>
       {children}
     </UserDashboardContext.Provider>
   )
